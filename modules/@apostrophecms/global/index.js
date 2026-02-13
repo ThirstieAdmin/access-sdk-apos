a9c6b0a30a07ff6a18047d96be96e0152a6f4db3
Bart Mosley <bart@shollermosley.com>
2026-02-13 17:12:06 -0500

    fix: apply custom styles

--- modules/@apostrophecms/global/index.js
+++ modules/@apostrophecms/global/index.js
@@ -171,6 +171,27 @@ export default {
         label: 'Footer text color',
         options: standardPresetColors
       },
+      footerFontFamily: {
+        type: 'string',
+        label: 'Footer font family',
+        help: 'Defaults to primary font family'
+      },
+      footerFontWeight: {
+        type: 'select',
+        label: 'Footer font weight',
+        help: 'Defaults to primary font weight',
+        choices: [
+          { value: '100', label: 'Thin (100)' },
+          { value: '200', label: 'Extra Light (200)' },
+          { value: '300', label: 'Light (300)' },
+          { value: '400', label: 'Normal (400)' },
+          { value: '500', label: 'Medium (500)' },
+          { value: '600', label: 'Semi Bold (600)' },
+          { value: '700', label: 'Bold (700)' },
+          { value: '800', label: 'Extra Bold (800)' },
+          { value: '900', label: 'Black (900)' }
+        ]
+      },
       /* Theme settings */
       primaryColor: {
         type: 'color',
@@ -498,7 +519,12 @@ export default {
       },
       footerContent: {
         label: 'Footer',
-        fields: [ 'footerLogo', 'footerTop', 'footerNav', 'footerBrandText', 'footerTrademarkText', 'footerBackgroundColor', 'footerTextColor', 'footerBackgroundImage', 'footerHeight' ]
+        fields: [
+          'footerLogo', 'footerTop', 'footerNav', 'footerBrandText', 'footerTrademarkText',
+          'footerBackgroundColor', 'footerTextColor', 'footerFontFamily', 'footerFontWeight',
+          'footerBackgroundImage',
+          'footerHeight'
+        ]
       },
       ageGate: {
         label: 'Age Gate',
