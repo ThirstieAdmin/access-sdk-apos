753953165171e32970dc75a242b904ff10f9d83f
Bart Mosley <bart@shollermosley.com>
2026-02-14 17:00:25 -0500

    feat: additional marquee widget content

--- modules/marquee-widget/index.js
+++ modules/marquee-widget/index.js
@@ -1,19 +1,29 @@
 import { standardPresetColors } from '../../lib/presets.js';
 import { default as richTextOptions } from '../../lib/rich-text-options.js';
+import { default as urlScheme } from '../../lib/url-scheme.js';
 
 export default {
   extend: '@apostrophecms/widget-type',
   options: {
     label: 'Marquee Widget',
-    icon: 'imageplus'
+    icon: 'image-plus'
   },
   // see list of icons in node_modules/@apostrophecms/vue-material-design-icons
   // or https://pictogrammers.com/library/mdi/
   icons: {
-    imageplus: 'ImagePlus'
+    'image-plus': 'ImagePlus'
   },
+  /* TODO: 
+  - add title, content and CTA fields.
+  - add side image option with image position (left or right) and text alignment (left, center, right) options.
+  */
   fields: {
     add: {
+      title: {
+        type: 'string',
+        label: 'Title',
+        help: 'Add a title to the marquee. This will appear above the caption.'
+      },
       caption: {
         type: 'area',
         options: {
@@ -21,6 +31,35 @@ export default {
             '@apostrophecms/rich-text': richTextOptions,
             '@apostrophecms/html': {}
           }
+        }
+      },
+      showCTA: {
+        type: 'boolean',
+        label: 'Show Call to Action',
+        def: false
+      },
+      ctaText: {
+        type: 'string',
+        label: 'Call to Action Text',
+        def: 'Learn More',
+        if: {
+          showCTA: true
+        }
+      },
+      ctaNav: {
+        type: 'array',
+        label: 'Call to Action navigation',
+        titleField: 'label',
+        options: {
+          max: 1
+        },
+        fields: {
+          add: {
+            ...urlScheme,
+          }
+        },
+        if: {
+          showCTA: true
         }
       },
       image: {
@@ -32,21 +71,40 @@ export default {
             '@apostrophecms/image': {}
           }
         }
-      },
-      textColor: {
-        type: 'color',
-        label: 'Text Color',
-        options: standardPresetColors
-      },
-      textFontFamily: {
-        type: 'string',
-        label: 'Text Font Family'
       }
     },
     group: {
       content: {
         label: 'Content',
-        fields: [ 'image', 'caption', 'textColor', 'textFontFamily' ]
+        fields: [ 'image', 'caption' ]
+      }
+    }
+  },
+  styles: {
+    add: {
+      backgroundColor: {
+        type: 'color',
+        label: 'Background Color',
+        help: 'Set the background color for the marquee. This will apply to the area behind the image and caption.',
+        selector: 'marquee-widget__container',
+        property: 'background-color',
+        def: 'inherit',
+        options: standardPresetColors
+      },
+      textColor: {
+        type: 'color',
+        label: 'Text Color',
+        help: 'Set the text color for the marquee.',
+        selector: 'marquee-widget__figure',
+        property: '--th-image-widget__caption-text-color',
+        options: standardPresetColors
+      },
+      textFontFamily: {
+        type: 'string',
+        label: 'Text Font Family',
+        selector: 'marquee-widget__figure',
+        property: '--th-image-widget__caption-font-family',
+        def: 'var(--primary-font-family)'
       }
     }
   }
