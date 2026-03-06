218cbd906bb87d7382bcbfb517fa2b62baf50288
Bart Mosley <bart@shollermosley.com>
2026-03-05 19:01:22 -0500

    feat: checkout styling

--- modules/thirstie-checkout/index.js
+++ modules/thirstie-checkout/index.js
@@ -3,13 +3,38 @@ export default {
   options: {
     label: 'Checkout',
     singleton: true
+  },
+  fields: {
+    add: {
+      continueButtonLabel: {
+        type: 'string',
+        label: 'continue-button-label',
+        def: 'Continue'
+      },
+      completeButtonLabel: {
+        type: 'string',
+        label: 'complete-button-label',
+        def: 'Complete Checkout'
+      },
+      confirmationMessage: {
+        type: 'string',
+        label: 'Message to show on confirmation page',
+        def: 'Thank you for shopping with us!'
+      }
+    },
+    group: {
+      content: {
+        label: 'Content',
+        fields: [ 'continueButtonLabel', 'completeButtonLabel', 'confirmationMessage' ]
+      }
+    }
   }
 };
 /*
 https://docs.apostrophecms.org/guide/pages.html#creating-a-page-type
 
 TODO:
-- add main widget
 - add slot content
 - options: opt-in, etc
+
 */
